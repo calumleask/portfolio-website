80cb8b941fd04a3273dd6b313b2c360bf85d70d4
calumleask <calum.leask@wrld3d.com>
2020-01-07 14:21:17 +0000

    Adding react-router-dom
    For page navigation

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+};
 
 export default class App extends React.Component {
 
@@ -8,7 +25,29 @@ export default class App extends React.Component {
 
     render () {
         return (
-            <div>Hello!</div>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                    </ul>
+
+                    <hr/>
+
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
