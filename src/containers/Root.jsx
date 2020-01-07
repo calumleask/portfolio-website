e2a103c1746de6f45c93802c59a54b5a02cd6002
calumleask <calum.leask@wrld3d.com>
2020-01-07 14:04:03 +0000

    Initial Commit
    Setting up a jekyll and react site

---
+++ src/containers/Root.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import App from "~/containers/App.jsx";
+
+export default class Root extends React.Component {
+    render() {
+        return (
+            <App/>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+var BUILD_DIR = path.resolve(__dirname, "dist");
+var APP_DIR = path.resolve(__dirname, "src");
+
+var config = {
+  entry: {
+    app: APP_DIR + "/index.jsx"
+  },
+
+  output: {
+    path: BUILD_DIR,
+    filename: "[name].js"
+  },
+
+  module : {
+    rules : [
+      {
+        test : /\.jsx?$/,
+        include : APP_DIR,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
