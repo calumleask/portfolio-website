54d846304aacaf05f3f367872f9b2bc9ccd18e9c
calumleask <cjleask@hotmail.co.uk>
2020-02-17 18:59:23 +0000

    Adding gatsby-ssr.js

---
+++ gatsby-ssr.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Layout from "src/components/layout";
+import WindowDimensionsProvider from "src/components/WindowDimensionsProvider";
+import ThemeProvider from "src/components/ThemeContext.jsx";
+
+export const wrapPageElement = ({ element, props }) => {
+    return <Layout {...props}>{element}</Layout>;
+};
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <WindowDimensionsProvider>
+            <ThemeProvider>
+                {element}
+            </ThemeProvider>
+        </WindowDimensionsProvider>
+    );
+};
